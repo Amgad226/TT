2b2bd57aac629aaa355ef9912796ac59095c57f7
Amgad226 <amgad.wr.1@gmail.com>
2022-11-03 00:56:35 +0200

    uploud to heroku

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,192.168.43.194:8000,192.168.1.36:8000,192.168.1.36:8000,192.168.1.36',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,192.168.43.194:8000,192.168.1.36:8000,192.168.1.36:8000,192.168.1.36,http://tt-226.herokuapp.com',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
