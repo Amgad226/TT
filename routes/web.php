5f608974bc8908c01f46ef80384bdc071d196103
Amgad226 <amgad.wr.1@gmail.com>
2023-03-14 04:26:20 +0200

    delete unnecessary files | clesn web.php from comments

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,10 @@
 <?php
 
-use App\Events\MessageCreated;
-use App\Http\Controllers\MessageController;
-use App\Http\Controllers\MessengerController;
-use App\Http\Controllers\PusherController;
-use App\Models\Message;
-use App\Models\User;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 
 
-
-// Auth::routes(['register' => false]);
-
-// Route::get('/a/{id?}',[MessengerController::class,'index'])->middleware('auth')->name('messenger');
-
-
-// Auth::routes(['except' => ['password.request', 'verification.notice', 'verification.verify']]);
-// Auth::routes(['register' => true, 'reset' => false, 'verify' => false, 'confirm' => false, 'except' => ['password.request', 'verification.notice', 'verification.verify']]);
-// Auth::routes(['reset' => false, 'verify' => false, 'confirm' => false]);
 Auth::routes(['register' => true, 'reset' => false, 'verify' => false, 'confirm' => false]);
 
 
@@ -32,8 +15,6 @@ Auth::routes(['register' => true, 'reset' => false, 'verify' => false, 'confirm'
 
 Route::view('/','messenger')->name('tt')->middleware('auth:sanctum');
 // Route::view('/a','welcome')->name('tt')->middleware('auth:sanctum');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::view('/404','404')->name('404');
